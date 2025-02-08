9275824a5fe6b8524dff1d7fbfa41a7718bd1917
Leas-y <theicewizard03@gmail.com>
2025-02-08 23:38:44 +0200

    1

--- js/task-4.js
+++ js/task-4.js
@@ -15,6 +15,7 @@ function getShippingCost(country) {
       break;
     case 'Jamaica':
       price = 120;
+      break;
     default:
       return 'Sorry, there is no delivery to your country';
   }
